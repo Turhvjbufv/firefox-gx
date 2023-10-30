76c04752f163a4a61feab5d52ea266b5eb9cda1e
Turhvjbufv <91851160+Turhvjbufv@users.noreply.github.com>
2023-10-30 20:17:18 +0200

    Update user.js

--- user.js
+++ user.js
@@ -9,7 +9,12 @@ user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
 user_pref("layout.css.backdrop-filter.enabled", true);
 user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
 
-/*To active container tabs without any extension */
-user_pref("privacy.userContext.enabled", true);
-user_pref("privacy.userContext.ui.enabled", true);
-user_pref("privacy.userContext.longPressBehavior", 2);
+/* Make it all false if you don't want other forms of tabs than default */
+user_pref("userChrome.tab.bottom_rounded_corner", true);
+
+/* Make it True (only one) the rule with the form of the tab you want */
+user_pref("userChrome.tab.bottom_rounded_corner.wave",          true);
+user_pref("userChrome.tab.bottom_rounded_corner.australis",     false);
+user_pref("userChrome.tab.bottom_rounded_corner.chrome",        false);
+user_pref("userChrome.tab.bottom_rounded_corner.chrome_legacy", false);
+user_pref("userChrome.tab.bottom_rounded_corner.edge",          false);
